bfb9a24b4a6060b7e5fbf945e119e62ea2e85d08
EcemAydn <ecemaydn15@gmail.com>
2023-04-26 17:09:30 +0300

    Create table component

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,2 +1,18 @@
-<div>
-</div>
+<template>
+  <div
+    class="w-full text-xs bg-background-default border border-border-default"
+  >
+    <table>
+      <thead class="bg-background-subtle-neutral">
+        <tr class="bg-interaction-ghost-normal">
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
